137111fd4dadf93cb314d2b35c6a1f24f17226e9
GiuliaZd <yuliya.zd@gmail.com>
2023-10-04 17:04:32 +0300

    Svelte app, setup

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import FeedbackList from "./components/FeedbackList.svelte";
+
+  let feedback = [
+    {
+      id: "1",
+      rating: "3",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde perferendis molestias sint alias dicta ratione rem cupiditate quae qui ea ex ad ullam, optio excepturi eos! Aspernatur, autem veritatis? Veniam?",
+    },
+    {
+      id: "2",
+      rating: "10",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde perferendis molestias sint alias dicta ratione rem cupiditate quae qui ea ex ad ullam, optio excepturi eos! Aspernatur, autem veritatis? Veniam?",
+    },
+    {
+      id: "3",
+      rating: "5",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde perferendis molestias sint alias dicta ratione rem cupiditate quae qui ea ex ad ullam, optio excepturi eos! Aspernatur, autem veritatis? Veniam?",
+    },
+  ];
+</script>
+
+<main>
+  <FeedbackList {feedback} />
+</main>
---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,6 @@
+<script>
+  export let item;
+  import Card from "./Card.svelte";
+</script>
+
+<Card>Hello</Card>
