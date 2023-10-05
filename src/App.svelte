54ea112e6398b72ed25dde81b044ac459938d934
GiuliaZd <yuliya.zd@gmail.com>
2023-10-05 16:39:10 +0300

    developed buttons, cards, feedback form, possibility to delete etc

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,38 @@
 <script>
   import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
+  import FeedbackForm from "./components/FeedbackForm.svelte";
 
   let feedback = [
     {
-      id: "1",
-      rating: "3",
+      id: 1,
+      rating: 10,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde perferendis molestias sint alias dicta ratione rem cupiditate quae qui ea ex ad ullam, optio excepturi eos! Aspernatur, autem veritatis? Veniam?",
     },
     {
-      id: "2",
-      rating: "10",
+      id: 2,
+      rating: 9,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde perferendis molestias sint alias dicta ratione rem cupiditate quae qui ea ex ad ullam, optio excepturi eos! Aspernatur, autem veritatis? Veniam?",
     },
     {
-      id: "3",
-      rating: "5",
+      id: 3,
+      rating: 8,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde perferendis molestias sint alias dicta ratione rem cupiditate quae qui ea ex ad ullam, optio excepturi eos! Aspernatur, autem veritatis? Veniam?",
     },
   ];
+
+  $: count = feedback.length;
+  $: average =
+    feedback.reduce((a, { rating }) => a + rating, 0) / feedback.length;
+
+  const deleteFeedback = (e) => {
+    const itemId = e.detail;
+    feedback = feedback.filter((item) => item.id != itemId);
+  };
 </script>
 
-<main>
-  <FeedbackList {feedback} />
+<main class="container">
+  <FeedbackForm />
+  <FeedbackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
